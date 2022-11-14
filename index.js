47c22392f1f2b6bc02929440c96c2aaad6086f1f
erquetefaka <88655548+erquetefaka@users.noreply.github.com>
2022-11-14 23:02:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require("./app");
 var port = 3700;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/portafolio").then(() =>{
+mongoose.connect("mongodb+srv://cjgvps:9667419990@cluster0.g18d7jv.mongodb.net/?retryWrites=true&w=majority").then(() =>{
     app.listen(port,() => {
         console.log("Conexión a la base de datos establecida con éxito corriendo en el puerto 3700.");
     });
